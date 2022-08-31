28166cb021e2d462ac779fc8437e33e41b7c3efe
ROHIT BISWAS <79495712+rohitproject123@users.noreply.github.com>
2022-08-31 20:37:24 +0530

    Add files via upload

---
+++ Myprogram1.java
@@ -0,0 +1,13 @@
+class DisplayMessage{
+ void printMessage(){
+   System.out.println("Hello My Message");
+}
+}  
+
+public class Myprogram1
+  {
+    public static void main(String arg[]){
+    	DisplayMessage program=new DisplayMessage();
+      program.printMessage();
+   }
+}
